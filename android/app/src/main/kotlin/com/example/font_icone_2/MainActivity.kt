78296cb5376456818727823ee7a8b154ccb1af36
Admin <makwanadharmishtha44@gmail.com>
2023-02-19 21:24:43 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/font_icone_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.font_icone_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:font_icone_2/screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {'/':(context) => Photo8()},
+    ),
+  );
+}
